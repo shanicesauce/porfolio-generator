a86e0d4d4d10534a9f6c3eec626c2562a33f8329
shanicesauce <shanicesauceda@gmail.com>
2022-09-06 15:03:55 -0500

    caputre input completed

---
+++ app.js
@@ -0,0 +1,13 @@
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+   profileDataArr.forEach(profileItem =>
+    console.log(profileItem)
+   );
+    
+};
+printProfileData(profileDataArgs)
