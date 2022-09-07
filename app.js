4bd95c42e94015c845d84a6986af5db0759da2d3
shanicesauce <shanicesauceda@gmail.com>
2022-09-07 18:36:30 -0500

    generated page

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var commandLineArgs = process.argv;
-console.log(commandLineArgs);
+const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-   profileDataArr.forEach(profileItem =>
-    console.log(profileItem)
-   );
-    
-};
-printProfileData(profileDataArgs)
+
+const [name,github] = profileDataArgs;
+
+const generatePage = require('./page-template')
+
+fs.writeFile('index.html',generatePage(name,github),err => {
+   if (err) throw err ; 
+
+   console.log('Portfolio complete! Checkout index.html to see the output!');
+})
